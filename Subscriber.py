914aa6555af3476794a9d7aff335f39fb19e6540
sorchanolan <32638110+sorchanolan@users.noreply.github.com>
2018-03-12 16:04:31 +0000

    Big old mess I need to hide for a bit

--- Subscriber.py
+++ Subscriber.py
@@ -1,11 +1,12 @@
 import paho.mqtt.client as mqtt
 from Crypto.Cipher import AES
+import binascii
 
 # Define Variables
 MQTT_HOST = "127.0.0.1"
 MQTT_PORT = 2883
 MQTT_KEEPALIVE_INTERVAL = 5
-MQTT_TOPIC = "topic"
+MQTT_TOPIC = "t"
 MQTT_MSG = "Hello MQTT"
 keys = ["Gv5BBQvjxDFNgjy", "rh4KTvALW6pyHRKr36yUcu4", "9PMkFNpjm7oikrhqYd3fEi9byIdz7GG"]
 
@@ -21,7 +22,17 @@ def on_subscribe(mosq, obj, mid, granted_qos):
 
 # Define on_message event Handler
 def on_message(mosq, obj, msg):
-	print msg.payload
+	s = msg.payload
+	h = binascii.hexlify(s)
+	print s
+	file2write=open("encrypted_data.txt",'a')
+	file2write.write(s + '\n')
+	file2write.close()
+
+	#decrypt(msg.payload)
+
+#def decrypt(encrypted_msg):
+#	aes = AES.new(keys[2], AES.MODE_CTR, )
 
 # Initiate MQTT Client
 mqttc = mqtt.Client()
@@ -36,3 +47,4 @@ mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL )
 
 # Continue the network loop
 mqttc.loop_forever()
+
